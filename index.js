045e338107a8b8e39862c3aeab124d6935074bf7
Kayla Hansen <shiverr@gmail.com>
2017-09-10 04:06:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ function nowServing(katzDeli) {
   return (`Currently serving ${katzDeli.shift()}.`);
 }
 
+var line = [];
+
 function currentLine(katzDeli) {
-  let i = 0;
-  while (i > katzDeli.length) {
-    i++;
-  return (`The line is currently: ${katzDeli.length}`);
-} if (katzDeli.length === 0) {
-    return (`The line is currently empty.`);
-  }
+for (let i = 0; i < katzDeli.length; i++) {
+line.push(` `+[i+1]+`. `  + katzDeli[i])
+}
+if (katzDeli.length === 0) {
+return "The line is currently empty.";
+} else
+return(`The line is currently:` + line);
 }
