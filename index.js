6e750fbc2e58ea90fa0c19354dce775ac886b888
Kayla Hansen <shiverr@gmail.com>
2017-09-10 02:46:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-function takeANumber (katzDeliLine, name) {
 var katzDeli = [];
-var linePosition = [];
-for (var i = 1; i > katzDeli.length; i++ ) {
-  linePosition.push(katzDeli[i]);
-}
-return linePosition;
+function takeANumber (katzDeliLine, name) {
+katzDeli.push(`${name}`);
+return(`Welcome, ${name}. You are ${katzDeli.length} in line.`);
 }
