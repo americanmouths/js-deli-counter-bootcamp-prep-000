144b99d8e1e618c68dcc8c6c58ee7767e3de924a
Kayla Hansen <shiverr@gmail.com>
2017-09-10 02:54:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function nowServing(katzDeli) {
     i++;
 } if (katzDeli.length === 0) {
   return (`There is nobody waiting to be served!`);
-} else 
+} else
   return (`Currently serving ${katzDeli.shift()}.`);
 }
